9794c51ead614764bc685b26ced7b27a9095fd2f
Md.Jobayer Hossain <hsifat18@gmail.com>
2021-08-07 14:40:53 +0600

    new line added

--- index2.js
+++ index2.js
@@ -1,12 +1,4 @@
-let n = 5; 
-let string = "";
-for (let i = 1; i <= n; i++) {
-  for (let j = 0; j < i; j++) {
-    string += "*";
-  }
-  string += "\n";
-}
-console.log(string);
+// 1. To find the largest of five numbers, create a JavaScript conditional statement. Print the result. ( only use conditional statement)
 
 var a = -5
 var b = -2
@@ -29,10 +21,7 @@ else{
   console.log(e);
 }
 
-
-var myColor = ["Red", "Green", "White", "Black"];
-console.log(myColor.join());
-console.log(myColor.join("+"));
+//3. Write a javascript program to add items to the beginning and delete from the beginning item of an array without using shift/unshift
 
 //adding item from the beginning
 var arr = [2,3,4,5]
@@ -44,15 +33,26 @@ var array = [1,2,3,4,5]
 array.splice(0,1)
 console.log(array)
 
-// function firstNonRepeatingCharacter(str) {
-//   for (let i = 0; i < str.length; i++) {
-//     let char = str[i];
-//     if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
-//       return char;
-//     }
-//   }
-// }
-// console.log(firstNonRepeatingCharacter("aaccthhdd"));
+//4. Write a simple JavaScript program to join all elements of the following array into a string.Sample array : myColor = ["Red", "Green", "White", "Black"];
+
+var myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.join());
+console.log(myColor.join("+"));
+
+
+//5. Write a JavaScript program to construct the following pattern, using a nested for loop. 
+let n = 5; 
+let string = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < i; j++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
+
+//6. Write a JavaScript function to find the first non-repeated character. Go to the editorSample arguments : 'abacddbec 'Expected output: 'e'
+
 function firstNonRepeatingCharacter(str) {
   for (let i = 0; i < str.length; i++) {
     if (str.indexOf(str[i]) === i && str.indexOf(str[i], i + 1) === -1) {
